8d6afd95503527debe8dba68b0b4413521c4042c
web <dekrement@gmail.com>
2017-03-09 15:33:40 +0300

    add data.sql

--- data.sql
+++ data.sql
@@ -1,17 +1,54 @@
-/*
--- Query: SELECT * FROM test.user
-LIMIT 0, 1000
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `test`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
 
--- Date: 2017-03-09 14:43
-*/
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (42,'Екатерина',33,'0','2017-03-07 00:00:00');
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (43,'Владислав',11,'0','2017-03-07 00:00:00');
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (44,'Валерий',44,'1','2017-03-07 00:00:00');
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (45,'Люда',15,'0','2017-03-07 00:00:00');
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (47,'Григорий',42,'0','2017-03-07 00:00:00');
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (48,'Кассиопей',51,'0','2017-03-07 00:00:00');
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (50,'Марат',41,'0','2017-03-07 00:00:00');
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (51,'Эльвира',10,'0','2017-03-07 00:00:00');
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (53,'Павел',30,'0','2017-03-07 00:00:00');
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (54,'Сергейё',14,'0','2017-03-09 00:00:00');
-INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (55,'Василий',100,'0','2017-03-09 00:00:00');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  `age` int(11) NOT NULL,
+  `isAdmin` bit(1) NOT NULL,
+  `createDate` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+INSERT INTO `user` VALUES (42,'Екатерина',33,'\0','2017-03-06 21:00:00'),(43,'Владислав',11,'\0','2017-03-06 21:00:00'),(44,'Валерий',44,'','2017-03-06 21:00:00'),(45,'Люда',15,'\0','2017-03-06 21:00:00'),(47,'Григорий',42,'\0','2017-03-06 21:00:00'),(48,'Кассиопей',51,'\0','2017-03-06 21:00:00'),(50,'Марат',41,'\0','2017-03-06 21:00:00'),(51,'Эльвира',10,'\0','2017-03-06 21:00:00'),(53,'Павел',30,'\0','2017-03-06 21:00:00'),(54,'Сергейё',14,'\0','2017-03-08 21:00:00'),(55,'Василий',100,'\0','2017-03-08 21:00:00');
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-09 15:32:59
