14dac602f623771d614e504279704cf4263a8f52
web <dekrement@gmail.com>
2017-03-09 14:45:14 +0300

    add data.sql

---
+++ data.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM test.user
+LIMIT 0, 1000
+
+-- Date: 2017-03-09 14:43
+*/
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (42,'Екатерина',33,'0','2017-03-07 00:00:00');
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (43,'Владислав',11,'0','2017-03-07 00:00:00');
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (44,'Валерий',44,'1','2017-03-07 00:00:00');
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (45,'Люда',15,'0','2017-03-07 00:00:00');
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (47,'Григорий',42,'0','2017-03-07 00:00:00');
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (48,'Кассиопей',51,'0','2017-03-07 00:00:00');
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (50,'Марат',41,'0','2017-03-07 00:00:00');
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (51,'Эльвира',10,'0','2017-03-07 00:00:00');
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (53,'Павел',30,'0','2017-03-07 00:00:00');
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (54,'Сергейё',14,'0','2017-03-09 00:00:00');
+INSERT INTO `user` (`id`,`name`,`age`,`isAdmin`,`createDate`) VALUES (55,'Василий',100,'0','2017-03-09 00:00:00');
